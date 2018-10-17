c0377978148f87fc02019f790fd0115e139e49b1
Sveta Busygina <sveta.busygina123@gmail.com>
2018-10-17 17:43:06 +0500

    Prompt user for last name as well.

--- main.c
+++ main.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 
 #include <string.h>
+
 int main(int argc, char **argv)
 
 {
 
-char name[255];
+char first[255], last[255];
 
-printf("Enter your name: ");
+printf("Enter your first name: ");
 
-fgets(name, 255, stdin);
+fgets(first, 255, stdin);
 
-printf("length = %d\n", strlen(name)); /* debug line */
+first[strlen(first)-1] = '\0'; /* remove the newline at the end */
 
-name[strlen(name)-1] = '\0'; /* remove the newline at the end */
+printf("Now enter your last name: ");
 
-printf("Hello %s!\n", name);
+gets(last); /* buffer overflow? what's that? */
+
+printf("Hello %s %s!\n", first, last);
 
 return 0;
 
